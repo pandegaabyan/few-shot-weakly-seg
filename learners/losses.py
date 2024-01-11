f7b5f0cee02f2762ddf1b1dd9553339ebd76dfef
pandega <pandegaabyan@gmail.com>
2024-01-11 08:11:29 +0700

    update loss calc

--- learners/losses.py
+++ learners/losses.py
@@ -1,31 +1,29 @@
-from abc import ABC, abstractmethod
-
+import torch
 import torch.nn as nn
 from torch import Tensor
 from torch.nn import functional
 
 
-class CustomLoss(nn.Module, ABC):
+class CustomLoss(nn.Module):
     def __init__(self):
         super(CustomLoss, self).__init__()
+        self.loss_type = 'ce'
+        self.ignored_index = -1
 
-    @abstractmethod
     def forward(self, inputs: Tensor, targets: Tensor) -> Tensor:
-        return functional.cross_entropy(inputs, targets, ignore_index=-1)
+        return functional.cross_entropy(inputs, targets, ignore_index=self.ignored_index)
+
+    def params(self) -> dict:
+        return {'loss_type': self.loss_type, 'ignored_index': self.ignored_index}
 
     @staticmethod
-    def iou_loss(inputs: Tensor, targets: Tensor, smooth=1):
+    def iou_loss(inputs: Tensor, targets: Tensor, smooth: int = 1):
         intersection = (inputs * targets).sum()
         union = (inputs + targets).sum() - intersection
         iou = (intersection + smooth)/(union + smooth)
         return 1 - iou
 
     @staticmethod
-    # TODO: currently unstable causing CUDA crash despite low memory usage
-    def iou_bce_loss(inputs: Tensor, targets: Tensor, smooth=1):
-        intersection = (inputs * targets).sum()
-        union = (inputs + targets).sum() - intersection
-        iou = (intersection + smooth)/(union + smooth)
-        bce = functional.binary_cross_entropy(inputs, targets)
-        iou_bce = (1 - iou) + bce
-        return iou_bce
+    def bce_loss(inputs: Tensor, targets: Tensor, ignore_index: int = -1):
+        expanded_inputs = torch.stack([inputs, 1 - inputs], dim=1)
+        return functional.cross_entropy(expanded_inputs, targets, ignore_index=ignore_index)
--- tasks/optic_disc_cup/losses.py
+++ tasks/optic_disc_cup/losses.py
@@ -17,23 +17,36 @@ class DiscCupLoss(CustomLoss):
         if self.loss_type == 'ce':
             return functional.cross_entropy(inputs, targets, ignore_index=self.ignored_index)
 
-        od_input, oc_input, od_target, oc_target = self.process_input_and_target(inputs, targets)
-
         if self.loss_type == 'iou':
+            od_input, oc_input, od_target, oc_target = self.process_input_and_target(inputs, targets)
             od_loss = self.iou_loss(od_input, od_target)
             oc_loss = self.iou_loss(oc_input, oc_target)
-        elif self.loss_type == 'iou_bce':
-            od_loss = self.iou_bce_loss(od_input, od_target)
-            oc_loss = self.iou_bce_loss(oc_input, oc_target)
-        else:
-            od_loss = functional.binary_cross_entropy(od_input, od_target)
-            oc_loss = functional.binary_cross_entropy(oc_input, oc_target)
+            return od_loss + oc_loss
 
-        return od_loss + oc_loss
+        if self.loss_type == 'bce':
+            od_input, oc_input, od_target, oc_target = self.split_od_oc(inputs, targets)
+            od_loss = self.bce_loss(od_input, od_target, ignore_index=self.ignored_index)
+            oc_loss = self.bce_loss(oc_input, oc_target, ignore_index=self.ignored_index)
+            return od_loss + oc_loss
 
-    def process_input_and_target(self, inputs: Tensor, targets: Tensor) -> tuple[Tensor, Tensor, Tensor, Tensor]:
-        od_input = functional.softmax(inputs, 1)[:, 1:3].sum(1)
-        oc_input = functional.softmax(inputs, 1)[:, 2]
+        if self.loss_type == 'iou_bce':
+            od_input, oc_input, od_target, oc_target = self.process_input_and_target(inputs, targets)
+            od_loss_iou = self.iou_loss(od_input, od_target)
+            oc_loss_iou = self.iou_loss(oc_input, oc_target)
+            od_input, oc_input, od_target, oc_target = self.split_od_oc(inputs, targets)
+            od_loss_bce = self.bce_loss(od_input, od_target, ignore_index=self.ignored_index)
+            oc_loss_bce = self.bce_loss(oc_input, oc_target, ignore_index=self.ignored_index)
+            return od_loss_iou + oc_loss_iou + od_loss_bce + oc_loss_bce
+
+        return 0
+
+    def params(self) -> dict:
+        return {'loss_type': self.loss_type, 'ignored_index': self.ignored_index}
+
+    @staticmethod
+    def split_od_oc(inputs: Tensor, targets: Tensor) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+        od_input = inputs[:, 1:3].sum(1)
+        oc_input = inputs[:, 2]
 
         od_target = torch.clone(targets)
         od_target[od_target == 2] = 1
@@ -41,9 +54,16 @@ class DiscCupLoss(CustomLoss):
         oc_target[oc_target == 1] = 0
         oc_target[oc_target == 2] = 1
 
+        return od_input, oc_input, od_target, oc_target
+
+    def process_input_and_target(self, inputs: Tensor, targets: Tensor) -> tuple[Tensor, Tensor, Tensor, Tensor]:
+        soft_inputs = functional.softmax(inputs, 1)
+
+        od_input, oc_input, od_target, oc_target = self.split_od_oc(soft_inputs, targets)
+
         od_input_filtered = od_input[od_target != self.ignored_index]
         oc_input_filtered = oc_input[oc_target != self.ignored_index]
-        od_target_filtered = od_target[od_target != self.ignored_index].type(torch.float32)
-        oc_target_filtered = oc_target[oc_target != self.ignored_index].type(torch.float32)
+        od_target_filtered = od_target[od_target != self.ignored_index]
+        oc_target_filtered = oc_target[oc_target != self.ignored_index]
 
         return od_input_filtered, oc_input_filtered, od_target_filtered, oc_target_filtered
