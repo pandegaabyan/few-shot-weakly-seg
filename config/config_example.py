3cbb2dcdfa74cecdd7a583977f94e8da8c1b44a8
pandega <pandegaabyan@gmail.com>
2024-01-28 11:37:53 +0700

    fix and update config example

--- config/config_example.py
+++ config/config_example.py
@@ -1,12 +1,14 @@
 from config.config_type import (
     ConfigAll,
     DataConfig,
-    DataTuneConfig,
+    GuidedNetsConfig,
     LearnConfig,
     LossConfig,
+    MetaLearnerConfig,
     OptimizerConfig,
     ProtoSegConfig,
     SchedulerConfig,
+    SimpleLearnerConfig,
     WeaselConfig,
 )
 
@@ -18,22 +20,10 @@ data_config: DataConfig = {
     "resize_to": (256, 256),
 }
 
-data_tune_config: DataTuneConfig = {
-    "shot_list": [1, 5, 10, 20],
-    "sparsity_dict": {
-        "point": [1, 5, 10, 20],
-        "grid": [8, 12, 16, 20],
-        "contour": [0.05, 0.10, 0.25, 0.50, 1.00],
-        "skeleton": [0.05, 0.10, 0.25, 0.50, 1.00],
-        "region": [0.05, 0.10, 0.25, 0.50, 1.00],
-    },
-}
-
 train_config: LearnConfig = {
     "should_resume": False,
     "use_gpu": True,
     "num_epochs": 200,
-    "tune_freq": 200,
     "exp_name": "",
 }
 
@@ -49,6 +39,20 @@ optimizer_config: OptimizerConfig = {
 
 scheduler_config: SchedulerConfig = {"step_size": 150, "gamma": 0.2}
 
+simple_learner_config: SimpleLearnerConfig = {"test_freq": 50}
+
+meta_learner_config: MetaLearnerConfig = {
+    "tune_freq": 50,
+    "shot_list": [1, 5, 10, 20],
+    "sparsity_dict": {
+        "point": [1, 5, 10, 20],
+        "grid": [8, 12, 16, 20],
+        "contour": [0.05, 0.10, 0.25, 0.50, 1.00],
+        "skeleton": [0.05, 0.10, 0.25, 0.50, 1.00],
+        "region": [0.05, 0.10, 0.25, 0.50, 1.00],
+    },
+}
+
 weasel_config: WeaselConfig = {
     "use_first_order": False,
     "update_param_step_size": 0.3,
@@ -58,13 +62,17 @@ weasel_config: WeaselConfig = {
 
 protoseg_config: ProtoSegConfig = {"embedding_size": 4}
 
+guidednets_config: GuidedNetsConfig = {"embedding_size": 4}
+
 config_all: ConfigAll = {
     "data": data_config,
-    "data_tune": data_tune_config,
     "learn": train_config,
     "loss": loss_config,
     "optimizer": optimizer_config,
+    "simple_learner": simple_learner_config,
+    "meta_learner": meta_learner_config,
     "scheduler": scheduler_config,
     "weasel": weasel_config,
     "protoseg": protoseg_config,
+    "guidednets": guidednets_config,
 }
