a058c2e98fa702a3d1ba41fca3c1a5ee3c6fdb1f
pandega <pandegaabyan@gmail.com>
2024-02-21 16:48:52 +0700

    update sweep count per agent

--- run.py
+++ run.py
@@ -143,7 +143,7 @@ def run_basic(
 def run_sweep(config: ConfigUnion, dummy: bool):
     sweep_config = {
         "method": "random",
-        "count_per_agent": 3,
+        "count_per_agent": 10,
         "parameters": {
             "opt_lr": {
                 "distribution": "log_uniform_values",
