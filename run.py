5a1c056972953905b89e16917d7e499c5b7caa88
pandega <pandegaabyan@gmail.com>
2024-07-20 10:45:11 +0700

    update study name

--- run.py
+++ run.py
@@ -72,7 +72,7 @@ class MyRunner(Runner):
 
     def make_optuna_config(self) -> OptunaConfig:
         config = super().make_optuna_config()
-        config["study_name"] = "Simple RIM-ONE BoTorch Median"
+        config["study_name"] = "Simple RIM-ONE BoTorch"
         config["sampler"] = "botorch"
         config["sampler_params"] = {
             "n_startup_trials": 20,
