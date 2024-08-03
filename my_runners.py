ce5301082ae98e43b841dbc9467247ffb5b6e72e
pandega <pandegaabyan@gmail.com>
2024-08-03 21:19:52 +0700

    try simple rim-one-3-train

--- my_runners.py
+++ my_runners.py
@@ -32,13 +32,12 @@ from tasks.optic_disc_cup.datasets import (
     DrishtiTestFSDataset,
     DrishtiTrainFSDataset,
     RefugeTestFSDataset,
-    RefugeTestSimpleDataset,
     RefugeTrainFSDataset,
-    RefugeTrainSimpleDataset,
     RefugeValFSDataset,
-    RefugeValSimpleDataset,
     RimOne3TestFSDataset,
+    RimOne3TestSimpleDataset,
     RimOne3TrainFSDataset,
+    RimOne3TrainSimpleDataset,
 )
 from tasks.optic_disc_cup.losses import DiscCupLoss
 from tasks.optic_disc_cup.metrics import DiscCupIoU
@@ -115,7 +114,7 @@ class SimpleRunner(Runner):
 
     def make_optuna_config(self) -> OptunaConfig:
         config = super().make_optuna_config()
-        config["study_name"] = "Simple REFUGE-train-val " + gen_id(5)
+        config["study_name"] = "Simple RIM-ONE-3-train " + gen_id(5)
         config["sampler_params"] = {
             "n_startup_trials": 20,
             "n_ei_candidates": 30,
@@ -132,8 +131,8 @@ class SimpleRunner(Runner):
         }
         config["pruner_patience"] = 5
         if not self.dummy:
-            config["num_folds"] = 1
-            config["timeout_sec"] = 250 * 60
+            config["num_folds"] = 3
+            config["timeout_sec"] = 120 * 60
         return config
 
     def make_dataset_lists(
@@ -184,13 +183,10 @@ class SimpleRunner(Runner):
 
         return {
             "dataset_list": [
-                (RefugeTrainSimpleDataset, refuge_train_kwargs),
-            ],
-            "val_dataset_list": [
-                (RefugeValSimpleDataset, refuge_val_kwargs),
+                (RimOne3TrainSimpleDataset, rim_one_3_train_kwargs),
             ],
             "test_dataset_list": [
-                (RefugeTestSimpleDataset, refuge_test_kwargs),
+                (RimOne3TestSimpleDataset, rim_one_3_test_kwargs),
             ],
         }
 
