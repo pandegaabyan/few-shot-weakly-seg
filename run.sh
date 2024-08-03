6ff3650c5350f34ebcceee91d1e7703f29a66a2f
pandega <pandegaabyan@gmail.com>
2024-08-03 20:49:37 +0700

    add sh file

---
+++ run.sh
@@ -0,0 +1,9 @@
+git checkout c61431c25bbf4534754203087a36949445fee498
+python run.py -m study -oc study_name 'Simple DRISHTI-GS train nf8p0'
+git checkout b51c970c20c9bf3c1f9a0aed386bebf36281ae0e
+python run.py -m study -oc study_name 'Simple RIM-ONE-3 train 8kcKT'
+git checkout 5a576f6e1ece2be47c037134a97982d3c2a084fc
+python run.py -m study -oc study_name 'Simple REFUGE train-val CzE0e'
+git checkout 95207bc4aeff3ab521075acb963a3f11f72817c2
+python run.py -m study -oc study_name 'Simple REFUGE val UDp6M'
+git checkout deployment
