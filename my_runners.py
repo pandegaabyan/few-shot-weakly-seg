2e6108d89f3d03b784f7cdbc6c25bbbf059b069a
pandega <pandegaabyan@gmail.com>
2024-09-13 17:00:59 +0700

    test ori weasel protoseg on rim-one-3

--- my_runners.py
+++ my_runners.py
@@ -28,12 +28,12 @@ from learners.weasel_learner import WeaselLearner
 from learners.weasel_unet import WeaselUnet
 from runners.runner import Runner
 from tasks.optic_disc_cup.datasets import (
-    DrishtiTestFSDataset,
     DrishtiTrainFSDataset,
     RefugeTestSimpleDataset,
     RefugeTrainFSDataset,
     RefugeValFSDataset,
     RefugeValSimpleDataset,
+    RimOne3TestFSDataset,
     RimOne3TrainFSDataset,
     drishti_sparsity_params,
     refuge_train_sparsity_params,
@@ -422,7 +422,7 @@ class MetaRunner(Runner):
                 (DrishtiTrainFSDataset, drishti_train_kwargs),
             ],
             "test_dataset_list": [
-                (DrishtiTestFSDataset, drishti_test_kwargs),
+                (RimOne3TestFSDataset, rim_one_3_test_kwargs),
             ],
         }
 
