fix confidence masking in pasnet for binary

learners/pasnet_learner.py
@@ -252,12 +252,12 @@ class PASNetLearner(MetaLearner[ConfigPASNet]):
         qry_pred_shape = qry_pred.shape  # [Q C H W] or [Q 1 H W] if binary
         if qry_pred_shape[1] == 1:
             qry_probs = torch.sigmoid(qry_pred).squeeze(1)  # [Q H W]
-            low_confidence_mask = qry_probs < self.high_confidence_threshold  # [Q H W]
+            max_probs = torch.max(qry_probs, 1 - qry_probs)  # [Q H W]
             qry_pred_label = (qry_probs > 0.5).type(torch.int64)  # [Q H W]
         else:
             qry_probs = F.softmax(qry_pred, dim=1)  # [Q C H W]
             max_probs, qry_pred_label = torch.max(qry_probs, dim=1)  # [Q H W]
-            low_confidence_mask = max_probs < self.high_confidence_threshold  # [Q H W]
+        low_confidence_mask = max_probs < self.high_confidence_threshold
         qry_pred_label[low_confidence_mask] = -1
 
         qry_pred_linear = qry_pred_label.view(