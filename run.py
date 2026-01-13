440f47cd40e90e090887d7358f195c6e30670e4c
pandega <pandegaabyan@gmail.com>
2026-01-13 17:26:23 +0900

    change run to optic disc cup

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ import click
 
 from config.config_maker import make_config
 from config.config_type import LearnerType, RunMode, learner_types, run_modes
-from tasks.skin_lesion.datasets import NUM_CLASSES
-from tasks.skin_lesion.runners import get_runner_class
+from tasks.optic_disc_cup.datasets import NUM_CLASSES
+from tasks.optic_disc_cup.runners import get_runner_class
 from utils.logging import (
     check_git_clean,
 )
