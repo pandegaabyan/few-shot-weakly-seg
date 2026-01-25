adbfad059409df442a8d3d8c1f784b463dcd7ae5
pandega <pandegaabyan@gmail.com>
2026-01-25 14:41:06 +0900

    update optuna config handling

--- runners/runner.py
+++ runners/runner.py
@@ -73,7 +73,10 @@ class Runner(ABC):
         self.run_name = self.config["learn"]["run_name"]
         self.seed = self.config["learn"].get("seed", 0)
 
-        self.optuna_config = self.make_optuna_config()
+        if mode == "study":
+            self.optuna_config = self.make_optuna_config()
+        else:
+            self.optuna_config: OptunaConfig = {}
         self.git_hash = get_short_git_hash()
         self.study_id = self.resolve_study_id()
 
@@ -267,6 +270,16 @@ class Runner(ABC):
             self.clean_log_on_end()
 
             return new_score
+
+        if (
+            "sampler" not in self.optuna_config
+            or "pruner" not in self.optuna_config
+            or "study_name" not in self.optuna_config
+            or "direction" not in self.optuna_config
+        ):
+            raise ValueError(
+                "Optuna config must have 'sampler', 'pruner', 'study_name', and 'direction' fields"
+            )
 
         sampler_class = sampler_classes[self.optuna_config["sampler"]]
         pruner_class = pruner_classes[self.optuna_config["pruner"]]
@@ -512,6 +525,8 @@ class Runner(ABC):
             wandb.finish()
             return
 
+        if "direction" not in self.optuna_config:
+            raise ValueError("Optuna config must have 'direction' field")
         minimize = self.optuna_config["direction"] == "minimize"
         try:
             best_score = trial.study.best_value
