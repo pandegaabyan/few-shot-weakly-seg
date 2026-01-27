4b7fa662fda618868ce94b271aac23ed1f1f5f01
pandega <pandegaabyan@gmail.com>
2026-01-27 11:59:15 +0900

    update optuna study name handling

--- tasks/skin_lesion/runners.py
+++ tasks/skin_lesion/runners.py
@@ -2,7 +2,6 @@ from typing import Any, Type
 
 import optuna
 
-from config.config_maker import gen_id
 from config.config_type import (
     ConfigMetaLearner,
     ConfigPANet,
@@ -190,7 +189,6 @@ class SimpleRunner(Runner):
 
     def make_optuna_config(self) -> OptunaConfig:
         config = super().make_optuna_config()
-        config["study_name"] = self.learner_type + " " + gen_id(5)
         config["sampler_params"] = {
             "n_startup_trials": 20,
             "n_ei_candidates": 30,
@@ -341,7 +339,6 @@ class MetaRunner(Runner):
 
     def make_optuna_config(self) -> OptunaConfig:
         config = super().make_optuna_config()
-        config["study_name"] = self.learner_type + " " + gen_id(5)
         config["sampler_params"] = {
             "n_startup_trials": 20,
             "n_ei_candidates": 30,
